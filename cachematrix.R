e03509be7b3f63a73462616d85f7f4ac0fa8f2ea
jagomezte <jagomezte@yahoo.com>
2016-02-04 12:42:19 -0600

    changes necessary for new version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this function creates a special vector (makeVector)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix = function(x1 = matrix()) {
+   n2 = NULL
+   set = function(y) {
+      x1 <<- y
+      n2 <<- NULL
+   }
+   get = function() x1
+   setmatrix = function(matrix) n2 <<- matrix
+   getmatrix = function() n2
+   list(set = set, get = get,
+        setmatrix = setmatrix,
+        getmatrix = getmatrix)
 }
 
+## calculates the mean of the special vector
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve = function(z, ...) {
+   p = x$getmatrix()
+   if(!is.null(p)) {
+      message("getting cached data")
+      return(p)
+   }
+   data = z$get()
+   p = try(solve(data, ...), silent = TRUE)
+   if(!is.matrix(p)) {
+       print(“this is not valid for inverse”)
+       
+   } else
+     {z$setmatrix(p)
+      p}
+ }
